e058e96659dfd241788bed9718ea20387a6a3af4
ionutpantazi <ionut28p@yahoo.com>
2020-04-21 22:26:37 +0100

    update mapbox

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,7 +7,29 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./content.component.scss'],
   providers: [NgbCarouselConfig]
 })
+
 export class ContentComponent implements OnInit {
+  
+  onClickMe(x,y,e: any) {
+    mapboxgl.accessToken = 'API KEY';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [x,y],
+      zoom: 17
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    map.scrollZoom.disable();
+    map.jumpTo({
+      center: [x, y],
+      essential: true // this animation is considered essential with respect to prefers-reduced-motion
+      });
+      new mapboxgl.Popup({ closeOnClick: false }).setHTML(
+        '<h6>'+e.target.textContent+'</h6>'
+        ).setLngLat([x, y]).addTo(map);
+        
+      
+  }
 
   constructor(config: NgbCarouselConfig) {
     config.interval = 5000;
@@ -18,5 +40,14 @@ export class ContentComponent implements OnInit {
     config.showNavigationIndicators = false;
   }
   ngOnInit(): void {
+    mapboxgl.accessToken = 'API KEY';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-1.2449378, 51.7517371],
+      zoom: 11.15
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    map.scrollZoom.disable();
   }
 }
